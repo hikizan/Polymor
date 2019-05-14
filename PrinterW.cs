ae4a5b23e3c7ff8717a8a5f453e673b88c49c86f
ozaneki <48448045+ozaneki@users.noreply.github.com>
2019-05-14 12:16:14 +0700

    Add files via upload

---
+++ PrinterW.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterWindows
+{
+    public class PrinterW
+    {
+        public string Name { get; set; }
+
+        public virtual void Print()
+        {
+            Console.Write.Line("printing .... ");
+        }
+
+        public virtual void Show()
+        {
+            Console.Write.Line(" .... ");
+        }
+
+    }
+}
